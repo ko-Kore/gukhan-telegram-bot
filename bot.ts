bcf9308b187d245c7dfcec6e7031976171b0721d
Hong Minhee <hong.minhee@gmail.com>
2022-07-08 02:34:19 +0900

    React to [[WikiLink]] syntax

--- bot.ts
+++ bot.ts
@@ -3,7 +3,6 @@ import {
   BotError,
   Context,
 } from "https://deno.land/x/grammy@v1.9.0/mod.ts";
-import { escapeHtml } from "https://deno.land/x/escape_html@1.0.0/mod.ts";
 import { Seonbi } from "https://deno.land/x/seonbi@0.3.1/mod.ts";
 
 const token = Deno.env.get("TELEGRAM_BOT_TOKEN");
@@ -27,6 +26,9 @@ const seonbi = new Seonbi({
   process: seonbiBinPath != null ? { binPath: seonbiBinPath } : "download",
 });
 
+const wikiApiUrl = new URL(
+  "https://wiki.xn--9cs231j0ji.xn--p8s937b.net/api.php",
+);
 const githubUrl = "https://github.com/ko-Kore/gukhan-telegram-bot";
 const agplv3Url = "https://www.gnu.org/licenses/agpl-3.0.html";
 
@@ -52,14 +54,15 @@ const helpMessage = `\
 求할 수 있습니다\\.
 `;
 
-async function help(ctx: Context) {
+async function help(ctx: Context): Promise<void> {
   await ctx.reply(helpMessage, {
     reply_to_message_id: ctx.msg?.message_id,
+    disable_notification: true,
     parse_mode: "MarkdownV2",
   });
 }
 
-async function hanjaToHangul(ctx: Context) {
+async function hanjaToHangul(ctx: Context): Promise<void> {
   const msg = (ctx.msg?.text ?? "");
   const text = msg.replace(/^\/\S+\s*/, "");
   if (text == "") {
@@ -86,6 +89,94 @@ async function hanjaToHangul(ctx: Context) {
   });
   await ctx.reply(hangul, {
     reply_to_message_id: ctx.msg?.message_id,
+    disable_notification: true,
+  });
+}
+
+const wikiLinkPattern = /\[\[(.+?)\]\]/g;
+
+async function wikiLink(ctx: Context): Promise<void> {
+  const msg = ctx.message?.text ?? "";
+  const titles: string[] = [];
+  for (const [_, title] of msg.matchAll(wikiLinkPattern)) {
+    titles.push(title);
+  }
+  const encodedTitles = titles.map(encodeURIComponent).join("|");
+  const infoApiUrl = new URL(
+    "?format=json&action=query&prop=info&inprop=url|displaytitle&titles=" +
+      encodedTitles,
+    wikiApiUrl,
+  );
+  const extractApiUrl = new URL(
+    "?format=json&action=query&prop=extracts&exintro=true&explaintext=true" +
+      "&exsentences=1&redirects&titles=" + encodedTitles,
+    wikiApiUrl,
+  );
+  const [infoResponse, extractResponse] = await Promise.all([
+    fetch(infoApiUrl),
+    fetch(extractApiUrl),
+  ]);
+  const infoResult: {
+    query: {
+      pages: Record<
+        string,
+        {
+          displaytitle: string;
+          title: string;
+          canonicalurl: string;
+          missing?: "";
+        }
+      >;
+    };
+  } = await infoResponse.json();
+  const extractResult: {
+    query: {
+      redirects?: [{ from: string; to: string }];
+      pages: Record<string, { title: string; extract: string }>;
+    };
+  } = await extractResponse.json();
+  const redirects = Object.fromEntries(
+    (extractResult.query?.redirects ?? [])
+      .map((pair) => [pair.to, pair.from]),
+  );
+  const extracts = Object.fromEntries(
+    Object.values(extractResult.query.pages).map((p) => [
+      redirects[p.title] ?? p.title,
+      p.extract,
+    ]),
+  );
+  const pages = Object.values(infoResult.query.pages);
+  pages.sort((a, b) => {
+    let aPos = titles.indexOf(a.title);
+    aPos = aPos < 0 ? titles.indexOf(a.displaytitle) : aPos;
+    aPos = aPos < 0 ? titles.length : aPos;
+    let bPos = titles.indexOf(b.title);
+    bPos = bPos < 0 ? titles.indexOf(b.displaytitle) : bPos;
+    bPos = bPos < 0 ? titles.length : bPos;
+    return aPos - bPos;
+  });
+  let replyHtml = "";
+  for (const page of pages) {
+    const tag = page.missing == null ? "b" : "s";
+    replyHtml += `[[<a href="${escapeHtml(page.canonicalurl)}"><${tag}>${
+      escapeHtml(page.displaytitle)
+    }</${tag}></a>]]`;
+    if (page.missing != null) {
+      replyHtml += " <s>(아직 없는 文書)</s>";
+    } else if (
+      extracts[page.title] != null ||
+      extracts[page.displaytitle] != null
+    ) {
+      const extract = extracts[page.title] ?? extracts[page.displaytitle];
+      if (extract.trim() !== "") replyHtml += `: ${escapeHtml(extract)}`;
+    }
+    replyHtml += "\n";
+  }
+  await ctx.reply(replyHtml, {
+    reply_to_message_id: ctx.msg?.message_id,
+    disable_notification: true,
+    disable_web_page_preview: true,
+    parse_mode: "HTML",
   });
 }
 
@@ -102,19 +193,44 @@ bot.on("message", async (ctx: Context) => {
     return await hanjaToHangul(ctx);
   } else if (ctx.message?.text?.startsWith("/도움말")) {
     return await hanjaToHangul(ctx);
+  } else if (ctx.message?.text?.match(wikiLinkPattern)) {
+    return await wikiLink(ctx);
   }
 });
 
 bot.catch(async (error: BotError) => {
   console.error(error);
-  await error.ctx.reply(
-    "豫想치 못한 誤謬가 發生했습니다.\n\n<code>" + escapeHtml(error.toString()) +
-      "</code></tg-spoiler>",
-    {
-      reply_to_message_id: error.ctx.msg?.message_id,
-      parse_mode: "HTML",
-    },
-  );
+  const msg = "豫想치 못한 誤謬가 發生했습니다.\n\n<code>" + escapeHtml(error.toString()) +
+    "</code>";
+  try {
+    await error.ctx.reply(
+      msg,
+      {
+        reply_to_message_id: error.ctx.msg?.message_id,
+        disable_notification: true,
+        parse_mode: "HTML",
+      },
+    );
+  } catch (e) {
+    console.debug("Error message:", JSON.stringify(msg));
+    throw e;
+  }
 });
+
+function escapeHtml(text: string): string {
+  return text.replaceAll(
+    /["<>&]/g,
+    (s) =>
+      s == "&"
+        ? "&amp;"
+        : s == "<"
+        ? "&lt;"
+        : s == ">"
+        ? "&gt;"
+        : s == '"'
+        ? "&quot;"
+        : s,
+  );
+}
 
 bot.start();
